c236d8e5b38bfbb51dfe212d694cbd56d98aa590
Marwa Eltayeb <marwa_eltayeb@yahoo.com>
2025-10-28 17:33:37 +0200

    Configure Firebase and add Crashlytics support

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:petfinder/core/routing/app_route.dart';
 import 'package:petfinder/core/routing/routes.dart';
 
 import 'core/di/injection_container.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await initAll();
   runApp(const MyApp());
 }
